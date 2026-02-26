29173a80de6c3889f278c9c2a32a84987ddaa8bf
Santu Mondal <mondalsantu104@gmail.com>
2026-02-26 23:53:23 +0530

    " feat : initial implement login ui "

--- frontend/job-portal/src/pages/Auth/Login.jsx
+++ frontend/job-portal/src/pages/Auth/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import {
+  Mail,
+  Lock,
+  Eye,
+  EyeOff,
+  Loader,
+  AlertCircle,
+  CheckCircle,
+} from "lucide-react";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    rememberMe: false,
+  });
+  const [formState, setFormState] = useState({
+    loading: false,
+    error: {},
+    showpassword: false,
+    success: false,
+  });
 
-export default Login
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+  const validatePassword = (password) => {};
+  const handleInputChange = (e) => {};
+  const validateForm = () => {};
+  const handleSubmit = async (e) => {};
+  return (
+    <div className="">
+            <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className=""
+        >
+          <div className="">
+            <h2 className="">Login to Your Account</h2>
+            <p className="">Welcome back! Please enter your details.</p>
+            </div>
+
+            <form onSubmit={handleSubmit} className="">
+              <div>
+                <label htmlFor="email" className="">Email address</label>
+                <div className="">
+                  <Mail className="" />
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className={`w-full pl-10 pr-4 py-3 rounded-lg border ${formState.error.email ? "border-red-500" : "border-gray-300"} focus:outline-none focus:ring-2 focus:ring-blue-500 focus:boreder-transparent transition-colors `}
+                    placeholder="Eneter your email"
+                  />
+                </div>
+                {formState.error.email && (
+                  <p className="">
+                    <AlertCircle className=""/>
+                    {formState.error.email}</p>
+                )}
+              </div>
+              
+            </form>
+
+          </div>
+        </motion.div>
+    </div>
+
+  )
+};
+
+export default Login;
