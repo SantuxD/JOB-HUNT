083672da944f372b4d23962ef4285524cd04b142
Santu Mondal <mondalsantu104@gmail.com>
2026-03-03 22:31:13 +0530

    "feat: auth middleware for protecting routes"

---
+++ backend/src/middlewares/auth.Middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.modals");
+
+const protect = async (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+
+    if (token && token.startsWith("Bearer")) {
+      token = token.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } else {
+      res.status(401).json({
+        message: "NOT Authorized, no token ",
+      });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Token failed", error: error.message });
+  }
+};
+
+module.exports =  protect 
