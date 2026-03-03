0b90d364c0e57dcaff634f70db6eca022929000a
Santu Mondal <mondalsantu104@gmail.com>
2026-03-03 22:32:39 +0530

    "feat: create upload middleware for protecting upload routes"

---
+++ backend/src/middlewares/upload.middleware.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()} - ${file.originalname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/jpg",
+    "application/pdf",
+  ];
+  cb(null, true);
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error("Only .jpeg, .jpg, .png, and .pdf formats are allowed"),
+      false,
+    );
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
