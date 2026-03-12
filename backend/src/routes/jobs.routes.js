96e643d44f64bf758843e857ff9fa835e6fec251
Santu Mondal <mondalsantu104@gmail.com>
2026-03-12 23:06:43 +0530

    "refactor: modify job routes"

--- backend/src/routes/jobs.routes.js
+++ backend/src/routes/jobs.routes.js
@@ -9,7 +9,7 @@ const {
   getJobsAdmin,
 } = require("../controllers/job.controller");
 
-const { protect } = require("../middlewares/auth.Middleware");
+const  protect  = require("../middlewares/auth.Middleware");
 
 const router = express.Router();
 
@@ -17,8 +17,8 @@ router.post("/create-jobs", protect, createJobs);
 router.get("/get-jobs", getJobs);
 router.put("/update-jobs", protect, updateJob);
 router.delete("/delete-jobs", protect, deleteJob);
-router.get("/id", getJobById);
-router.get("get-jobs-admin", protect, getJobsAdmin);
+router.get("/:id", getJobById);
+router.get("/get-jobs-admin", protect, getJobsAdmin);
 router.put("/:id/toggle-close", protect, toggleCloseJob);
 
 module.exports = router;
