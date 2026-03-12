4782aebbf470c1d42aaeee08aa20244e7934fd80
Santu Mondal <mondalsantu104@gmail.com>
2026-03-12 23:08:02 +0530

    "feat: initialize createjobs and getjobs function"

---
+++ backend/src/controllers/job.controller.js
@@ -0,0 +1,138 @@
+const jobModal = require("../models/Job.modals");
+const userModal = require("../models/User.modals");
+const ApplicationModal = require("../models/Application.modals");
+const savedJobModal = require("../models/SavedJob.modals");
+const jwt = require("jsonwebtoken");
+
+const createJobs = async (req, res) => {
+  try {
+
+    if (req.user.role !== "admin") {
+      return res.status(403).json({
+        message: "only admin can post the job" + err.message,
+      });
+    }
+
+    const job = await jobModal.create({ ...req.body, company: req.user._id });
+    res.status(201).json(job);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for create job" + err.message,
+    });
+  }
+};
+
+const getJobs = async (req, res) => {
+  const { keyword, location, category, type, minSalary, maxSalary, userId } =
+    req.query;
+
+  const query = {
+    isClosed: false,
+    ...(keyword && { title: { $regex: keyword, $options: "i" } }),
+    ...(location && { location: { $regex: location, $options: "i" } }),
+    ...(category && { category }),
+    ...(type && { type }),
+  };
+  if (minSalary || maxSalary) {
+    query.$and = [];
+
+    if (minSalary) {
+      query.$and.push({ salaryMax: { $gte: Number(minSalary) } });
+    }
+
+    if (maxSalary) {
+      query.$and.push({ salaryMin: { $lte: Number(maxSalary) } });
+    }
+    if (query.$and.length === 0) {
+      delete query.$and;
+    }
+  }
+
+  try {
+    const jobs = await jobModal
+      .find(query)
+      .populate("company", "name companyName, companyLogo");
+    let savedJobIds = [];
+    let appliedJobStatusMap = {};
+
+    if (userId) {
+      const savedJobs = await savedJobModal
+        .find({ jobseeker: userId })
+        .select("job");
+      savedJobIds = savedJobs.map((s) => String(s.job));
+
+      const applications = await ApplicationModal.find({
+        applicant: userId,
+      }).select("job status");
+      applications.forEach((app) => {
+        appliedJobStatusMap[String(app.job)] = app.status;
+      });
+    }
+
+    const jobWithExtras = jobs.map((job) => {
+      const jobIdstr = String(job._id);
+      return {
+        ...job.toObject(),
+        isSaved: savedJobIds.includes(jobIdstr),
+        applicationStatus: appliedJobStatusMap[jobIdstr] || null,
+      };
+    });
+
+    res.json(jobWithExtras);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for getting job" + err.message,
+    });
+  }
+};
+
+const getJobById = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for getting jobs with ID" + err.message,
+    });
+  }
+};
+const updateJob = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for updating jobs" + err.message,
+    });
+  }
+};
+const deleteJob = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for delete jobs" + err.message,
+    });
+  }
+};
+const toggleCloseJob = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for closejobs" + err.message,
+    });
+  }
+};
+const getJobsAdmin = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: "Error for getting admin job" + err.message,
+    });
+  }
+};
+
+module.exports = {
+  createJobs,
+  getJobById,
+  getJobs,
+  updateJob,
+  deleteJob,
+  toggleCloseJob,
+  getJobsAdmin,
+};
