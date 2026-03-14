987b3a65ab9ee5a4181115897728a8e3b4074bab
Santu Mondal <mondalsantu104@gmail.com>
2026-03-15 00:22:49 +0530

    "refactor : modify auth middleware"

--- backend/src/middlewares/auth.Middleware.js
+++ backend/src/middlewares/auth.Middleware.js
@@ -9,7 +9,6 @@ const protect = async (req, res, next) => {
       token = token.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
       req.user = await User.findById(decoded.id).select("-password");
-
       next();
     } else {
       res.status(401).json({
@@ -21,4 +20,4 @@ const protect = async (req, res, next) => {
   }
 };
 
-module.exports =  protect 
+module.exports = protect;
