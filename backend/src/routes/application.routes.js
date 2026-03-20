cdea60dfb54c158e70c73187116eaeb07f007b06
Santu Mondal <mondalsantu104@gmail.com>
2026-03-20 23:58:14 +0530

    "feat: add features in application routes"

--- backend/src/routes/application.routes.js
+++ backend/src/routes/application.routes.js
@@ -1,1 +1,9 @@
 const express = require("express");
+
+const {applyToJob, getMyapplication,getApplicantsForJob, getApplicationById, updateStatus} = require("../controllers/application.controller")
+
+const {protect} = require("../middlewares/auth.Middleware")
+
+const router = express.Router();
+
+router.post("")
