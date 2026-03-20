0824bfe8d86258b53d7456da11f58c2fc182b315
Santu Mondal <mondalsantu104@gmail.com>
2026-03-21 00:04:43 +0530

    "refactor : modify application routes"

--- backend/src/routes/application.routes.js
+++ backend/src/routes/application.routes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
-const {applyToJob, getMyapplication,getApplicantsForJob, getApplicationById, updateStatus} = require("../controllers/application.controller")
+const {
+  applyToJob,
+  getMyApplications,
+  getApplicantsForJob,
+  getApplicationById,
+  updateStatus,
+} = require("../controllers/application.controller");
 
-const {protect} = require("../middlewares/auth.Middleware")
+const { protect } = require("../middlewares/auth.Middleware");
 
 const router = express.Router();
 
-router.post("")
+router.post("/:jobId", protect, applyToJob);
+router.get("/my", protect, getMyApplications);
+router.get("/job/:jobId", protect, getApplicantsForJob);
+router.get("/:id", protect, getApplicationById);
+router.put("/:id/status", protect, updateStatus);
+
+module.exports = router;
