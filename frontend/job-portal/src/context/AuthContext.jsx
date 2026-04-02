d63c164034c3a13d2505dab4e0812e2a01fe599e
Santu Mondal <mondalsantu104@gmail.com>
2026-04-02 23:28:44 +0530

    "feat : create authContext"

--- frontend/job-portal/src/context/AuthContext.jsx
+++ frontend/job-portal/src/context/AuthContext.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import { createContext, useContext, useState, useEffect, use } from "react";
 
-const AuthContext = () => {
-  return (
-    <div>AuthContext</div>
-  )
-}
+export const AuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
 
-export default AuthContext
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    checkAuthstatus();
+  }, []);
+
+  const checkAuthstatus = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const userStr = localStorage.getItem("user");
+      if (token && userStr) {
+        const userData = setUser(JSON.parse(userStr));
+        setUser(userData);
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      console.error("Error checking auth status:", error);
+      logout();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const login = async (userData, token) => {
+    try {
+      localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(userData));
+      setUser(userData);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("Error during login:", error);
+      logout();
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("user");
+    setUser(null);
+    setIsAuthenticated(false);
+    window.location.href = "/";
+  };
+
+  const updateUser = (updatedData) => {
+    const updatedUser = { ...user, ...updatedData };
+    setUser(updatedUser);
+    localStorage.setItem("user", JSON.stringify(updatedUser));
+    setUser(updatedUser);
+
+  };
+
+  const value = {
+    user,
+    loading,
+    isAuthenticated,
+    login,
+    logout,
+    updateUser,
+    checkAuthstatus,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
