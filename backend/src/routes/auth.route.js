558a21b7c84b1e4a596251a3893ad8e9b1a88fda
Santu Mondal <mondalsantu104@gmail.com>
2026-04-06 22:54:33 +0530

    "refactor : modify auth route '"

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const protect = require("../middlewares/auth.Middleware");
 const { register, login, getMe } = require("../controllers/auth.controller");
+const upload = require("../middlewares/upload.middleware");
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", upload.single("avatar"), register);
 router.post("/login", login);
 router.get("/getMe", protect, getMe);
 
