5902abb71c3ba64ef97304c33a01a2b92c297065
Santu Mondal <mondalsantu104@gmail.com>
2026-04-15 23:26:25 +0530

    "refactor: modify admin dashboard"

--- frontend/job-portal/src/pages/Admin/AdminDashboard.jsx
+++ frontend/job-portal/src/pages/Admin/AdminDashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import {
+  Plus,
+  Briefcase,
+  Users,
+  Building2,
+  TrendingUp,
+  CheckCircle2,
+} from "lucide-react";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
+import DashboardLayout from "../../components/layout/DashboardLayout";
 
 const AdminDashboard = () => {
-  return (
-    <div>AdminDashboard</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default AdminDashboard
+  const [dashboardData, setDashboardData] = useState(null);
+  const [isloading, setIsLoading] = useState(false);
+
+  const getDashboardOverview = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosInstance.get(API_PATHS.DASHBOARD.OVERVIEW);
+      if (response.status === 200) {
+        setDashboardData(response.data);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error("Error fetching dashboard overview:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardOverview();
+    return () => {
+      setDashboardData(null);
+    };
+  }, []);
+
+  return <DashboardLayout activeMenu="admin-dashboard"></DashboardLayout>;
+};
+
+export default AdminDashboard;
