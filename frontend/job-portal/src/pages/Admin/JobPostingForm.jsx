71f15e61c82576ebb3b2176ac3968f83431ba8ee
Santu Mondal <mondalsantu104@gmail.com>
2026-05-10 22:29:53 +0530

    feat: add initial scaffold for JobPostingForm component in admin dashboard

--- frontend/job-portal/src/pages/Admin/JobPostingForm.jsx
+++ frontend/job-portal/src/pages/Admin/JobPostingForm.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import DashboardLayout from "../../components/layout/DashboardLayout"
+import { AlertCircle, Briefcase, DollarSign, Eye, MapPin, Send, Users } from "lucide-react"
+import {API_PATHS} from "../../utils/apiPath"
+
 
 const JobPostingForm = () => {
   return (
-    <div>JobPostingForm</div>
+   <DashboardLayout activeMenu= 'post-job'>JobPostingForm</DashboardLayout>
   )
 }
 
