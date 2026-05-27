2a22633eb6c5c1d535a2e987f6e8556cbcff3698
Santu Mondal <mondalsantu104@gmail.com>
2026-05-27 19:50:03 +0530

    "feat: implement application dashboard card"

--- frontend/job-portal/src/components/cards/ApplicationDashboardCard.jsx
+++ frontend/job-portal/src/components/cards/ApplicationDashboardCard.jsx
@@ -4,18 +4,26 @@ const ApplicationDashboardCard = ({ applicant, position, time }) => {
   return (
     <div className="flex items-center justify-between p-4 rounded-xl border border-gray-100 hover:border-gray-200 transition-colors">
       <div className="flex items-center space-x-4">
-        <div className="h-10 w-10 bg-linear-to-br from-indigo-400 to-indigo-500 rounded-xl flex items-center justify-center ">
-          <span className="text-white font-medium text-sm">
-            {applicant?.name
-              ?.split(" ")
-              ?.slice(0, 2)
-              ?.map((n) => n[0]?.toUpperCase())
-              ?.join("") || ""}
-          </span>
-        </div>
+        {applicant?.avatar ? (
+          <img
+            src={applicant?.avatar}
+            alt={applicant?.fullName || applicant?.name || "Applicant"}
+            className="h-10 w-10 object-cover rounded-xl"
+          />
+        ) : (
+          <div className="h-10 w-10 bg-linear-to-br from-indigo-400 to-indigo-500 rounded-xl flex items-center justify-center ">
+            <span className="text-white font-medium text-sm">
+              {(applicant?.fullName || applicant?.name)
+                ?.split(" ")
+                ?.slice(0, 2)
+                ?.map((n) => n[0]?.toUpperCase())
+                ?.join("") || ""}
+            </span>
+          </div>
+        )}
         <div>
           <h4 className="text-[15px] font-medium text-gray-900">
-            {applicant?.name || "Unknown"}
+            {applicant?.fullName || applicant?.name || "Unknown"}
           </h4>
           <p className="text-sm text-gray-500">{position}</p>
         </div>
