c2665a1ed09a54c8e2ab1779e8b6b4ecfbcc13ab
Santu Mondal <mondalsantu104@gmail.com>
2026-05-27 20:09:11 +0530

    "reafactor: modify api path"

--- frontend/job-portal/src/utils/apiPath.js
+++ frontend/job-portal/src/utils/apiPath.js
@@ -4,9 +4,9 @@ export const API_PATHS = {
   AUTH: {
     LOGIN:"/api/auth/login",
     REGISTER: "/api/auth/register",
-    GET_PROFILE: "/api/auth/profile",
-    UPDATE_PROFILE: "/api/auth/profile/update",
-    DELETE_RESUME: "/api/resume/delete",
+    GET_PROFILE: "/api/auth/getMe",
+    UPDATE_PROFILE: "/api/user/profile",
+    DELETE_RESUME: "/api/user/resume",
   },
 
   DASHBOARD: {
@@ -21,18 +21,18 @@ export const API_PATHS = {
     UPDATE_JOB: (id) => `/api/jobs/${id}`,
     TOGGLE_CLOSE_JOB: (id) => `/api/jobs/${id}/toggle-close`,
     DELETE_JOB: (id) => `/api/jobs/${id}`,
-    SAVE_JOB: (id) => `/api/save-jobs/${id}`,
-    UNSAVE_JOB: (id) => `/api/save-jobs/${id}`,
-    GET_SAVED_JOBS: "/api/save-jobs/my",
+    SAVE_JOB: (id) => `/api/saved-jobs/${id}`,
+    UNSAVE_JOB: (id) => `/api/saved-jobs/${id}`,
+    GET_SAVED_JOBS: "/api/saved-jobs/my",
   },
 
   APPLICATIONS: {
-    APPLY_TO_JOB: (id) => `/api/applications/${id}`,
-    GET_ALL_APPLICATIONS: (id) => "/api/applications/job/${id}",
-    UPDATE_STATUS: (id) => `/api/applications/${id}/status`,
+    APPLY_TO_JOB: (id) => `/api/application/${id}`,
+    GET_ALL_APPLICATIONS: (id) => `/api/application/job/${id}`,
+    UPDATE_STATUS: (id) => `/api/application/update-status/${id}`,
   },
 
   IMAGE: {
-    UPLOAD: "/api/auth/upload-image",
+    UPLOAD: "/api/v1/upload-image",
   },
 };
