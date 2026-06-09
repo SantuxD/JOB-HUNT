22e2925a10c1a09db0397994fa3e32f94b629826
Santu Mondal <mondalsantu104@gmail.com>
2026-06-09 20:35:31 +0530

    "fix baseurl"

--- frontend/job-portal/src/utils/axiosInstance.js
+++ frontend/job-portal/src/utils/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { BASE_URL } from "./apiPath";
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: VITE_API_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
